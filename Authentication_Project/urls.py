d06d00dd999b18ad95748956bea3168822cf97f6
zahidhasanmilu <minions.milu889@gmail.com>
2025-03-15 03:26:29 +0600

    home, layout templates setup

--- Authentication_Project/urls.py
+++ Authentication_Project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_account.urls'))
+    path('', include('app_account.urls')),
+    path('', include('app_home.urls'))
 ]
